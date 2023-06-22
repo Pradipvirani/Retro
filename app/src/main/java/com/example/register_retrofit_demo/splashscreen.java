68a15efe54bdeac2c84a06419acb44fb75f40a60
HP <pureb88@gmail.com>
2023-06-22 18:04:28 +0530

    three

---
+++ app/src/main/java/com/example/register_retrofit_demo/splashscreen.java
@@ -0,0 +1,15 @@
+package com.example.register_retrofit_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splashscreen extends AppCompatActivity {
+    private 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
